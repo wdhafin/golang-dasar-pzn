c23a2e03131c3c8d515e46176d9e51114e3aab18
Muhammad Dhafin Rizqullah <work.dhafin@gmail.com>
2020-10-16 00:12:56 +0700

    Add string data type

---
+++ string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Muhammad")
+	fmt.Println("Muhammad Dhafin")
+	fmt.Println("Muhammad Dhafin Rizqullah")
+
+	// len and index
+	fmt.Println(len("Muhammad"))
+	fmt.Println("Muhammad Dhafin"[0])
+	fmt.Println("Muhammad Dhafin Rizqullah"[1])
+}
