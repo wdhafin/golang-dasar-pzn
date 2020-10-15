0fe0af7a559109df89f2e46df54d90b1785826d4
Muhammad Dhafin Rizqullah <work.dhafin@gmail.com>
2020-10-16 00:26:39 +0700

    Add variable example

---
+++ variable.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Muhammad Dhafin"
+	fmt.Println(name)
+
+	name = "Dhafin Rizqullah"
+	fmt.Println(name)
+
+	var friendName = "Budi"
+	fmt.Println(friendName)
+
+	var age = 30
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Dhafin"
+		lastName  = "Rizqullah"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
