7c0c372f08b17e0f0ae5bf13611209c0eefd31ca
Muhammad Dhafin Rizqullah <work.dhafin@gmail.com>
2020-10-20 22:49:28 +0700

    Add Comparation Operator Example

---
+++ comparation-operator.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name1 = "Muhammad"
+	var name2 = "Dhafin"
+
+	var result bool = name1 == name2
+	fmt.Println(result)
+
+	var value1 = 100
+	var value2 = 200
+
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
+}
