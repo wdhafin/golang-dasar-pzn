27be3ccd916acf60698bcd7f5287fd0e0705ec77
Muhammad Dhafin Rizqullah <work.dhafin@gmail.com>
2020-10-21 23:24:52 +0700

    Add array example

---
+++ array.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var names [3]string
+
+	names[0] = "Muhammad"
+	names[1] = "Dhafin"
+	names[2] = "Rizqullah"
+
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+
+	var values = [3]int{
+		90,
+		95,
+		80,
+	}
+
+	fmt.Println(values)
+	fmt.Println(values[0])
+	fmt.Println(values[1])
+	fmt.Println(values[2])
+
+	// get array length
+	fmt.Println(len(names))
+	fmt.Println(len(values))
+
+	var lagi [10]string
+
+	fmt.Println(len(lagi))
+}
