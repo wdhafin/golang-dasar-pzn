591546b73fab3c0e5ce51bde86843d28e8b5bdea
Muhammad Dhafin Rizqullah <work.dhafin@gmail.com>
2020-10-22 23:20:26 +0700

    Add if statement Example

---
+++ if.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Dhafin"
+
+	if name == "Dhafin" {
+		fmt.Println("Hello Dhafin")
+	} else if name == "Joko" {
+		fmt.Println("Hello Joko")
+	} else if name == "Budi" {
+		fmt.Println("Hello Budi")
+	} else {
+		fmt.Println("Hi, kenalan donk")
+	}
+
+	if length := len(name); length > 5 {
+		fmt.Println("Terlalu Panjang")
+	} else {
+		fmt.Println("Nama Sudah Benar")
+	}
+}
